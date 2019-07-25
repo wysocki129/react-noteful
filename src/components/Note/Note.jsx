8180369d41a75e18108b12992c1cf91014a0c978
Chris Wysocki <christopher.wysocki@gmail.com>
2019-07-25 15:56:52 -0700

    added toggle state for Note Content

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Note = ({ data }) => {
+	const [isContentActive, setIsContentActive] = useState(false);
+
+	const showContent = ({ isContentActive }) => {
+		let whatToShow =
+			isContentActive === true ? (
+				<>
+					<p>{data.content}</p>
+					<button
+						type="button"
+						onClick={() => setIsContentActive(false)}
+					>
+						Hide Content
+					</button>
+				</>
+			) : (
+				<button
+					type="button"
+					onClick={() => setIsContentActive(true)}
+				>
+					Show Content
+				</button>
+			);
+		return whatToShow;
+	};
+
 	return (
 		<div className="Note" key={data.id} id={data.id}>
 			<Link to={`/folder/${data.folderId}/note/${data.id}`}>
 				<h3> {data.name} </h3>
 			</Link>
 			<p>{data.modified}</p>
-			<p>{data.content}</p>
+			{showContent({ isContentActive })}
 			<button
 				type="button"
 				onClick={() => console.log('Note Deleted')}
